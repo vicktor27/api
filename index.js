356b18c0fabc76fb593a155860ced6bb16bdd4fc
Vickthor3T <victor.cardoza@tripleten-team.com>
2025-05-21 15:23:15 -0600

    initial__commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/temperatura", (req, res) => {
+  res.json({ valor: "10ºC", timestamp: new Date().toISOString() });
+});
+
+app.get("/esteesotroendppoint", (req, res) => {
+  res.json({ valor: "HOLA MUNDO" });
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto" + PORT);
+});
